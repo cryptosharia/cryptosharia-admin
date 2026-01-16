52feb51f0ac625850e0d6c0bc2cad42224aad89c
Daffa Ilhami <mdaffailhami@gmail.com>
2026-01-16 21:29:35 +0800

    chore(project): TypeScriptify the project

--- eslint.config.js
+++ eslint.config.js
@@ -3,29 +3,41 @@ import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
+import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-/** @type {import('eslint').Linter.Config[]} */ export default [
+export default defineConfig(
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
+	...ts.configs.recommended,
 	...svelte.configs.recommended,
 	prettier,
 	...svelte.configs.prettier,
-
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 
 		rules: {
-			// Uncomment this if the project has many external links
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off'
+			// Uncomment this (below) if the project has many external links
 			// 'svelte/no-navigation-without-resolve': 'off'
 		}
 	},
-
 	{
-		files: ['**/*.svelte', '**/*.svelte.js'],
-		languageOptions: { parserOptions: { svelteConfig } }
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig
+			}
+		}
 	}
-];
+);
